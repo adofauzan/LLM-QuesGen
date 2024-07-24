0b12ed61727c673f06d46a9c5047aece9d1b80d0
adofauzan <98723541+adofauzan@users.noreply.github.com>
2024-07-24 13:10:06 +0700

    Update llm_handler.py

--- llm_handler.py
+++ llm_handler.py
@@ -1,78 +1,102 @@
 from langchain_community.llms.huggingface_pipeline import HuggingFacePipeline
-from langchain.prompts import ChatPromptTemplate
-from question_type import MultipleAnswer, MultipleChoice, Essay
+from langchain.prompts import ChatPromptTemplate, PromptTemplate,MessagesPlaceholder
 from langchain_core.output_parsers import JsonOutputParser
+from question_type import MultipleAnswer, SingleAnswer, Essay
+from langchain_core.pydantic_v1 import BaseModel, Field
 
-templates = (
-    ("english", [("system", """You are a quiz generator. Your job is to create question to test the competency of the question taker. When creating the question, ensure that the question demands critical thinking and an in-depth understanding of the subject matter. Additionally, provide detailed answers for each question to offer clarity and accuracy in evaluation.
-Use the following pieces of context along with your already known knowledge to create the question.
-If the given context does not help in creating the question, don't use it. Don't change the subject of the created question from the subject asked.
-{context}"""),
-("human", "Make {amount} {q_type} question about the subject {subject}. Write it in {language}.{extra}"),
-("system", "Format Instruction:{format_instructions}")
-]),
-    ("indonesia", [("system", """Anda adalah generator kuis. Tugas Anda adalah membuat pertanyaan untuk menguji kompetensi peserta kuis. Saat membuat pertanyaan, pastikan bahwa pertanyaan tersebut menuntut pemikiran kritis dan pemahaman mendalam tentang materi yang ditanyakan. Selain itu, berikan jawaban terperinci untuk setiap pertanyaan untuk memberikan kejelasan dan akurasi dalam evaluasi.
-Gunakan potongan konteks berikut bersama dengan pengetahuan yang sudah Anda ketahui untuk membuat pertanyaan.
-Jika konteks yang diberikan tidak membantu dalam membuat pertanyaan, jangan gunakan konteks tersebut. Jangan mengubah subjek pertanyaan yang dibuat dari subjek yang diminta.
-{context}"""),
-("human", "Buatlah {amount} pertanyaan {q_type} tentang subjek {subject}. Tulis dalam bahasa {language}.{extra}"),
-("system", "Instruksi Format:{format_instructions}")
-    ])
-)
+template = [
+        ("system", """You are a quiz generator that create questions. 
+        You are tasked to create {amount} questions based on the subjects asked.
+        Create the question in {language}
+        The questions created are based on the model pre-knowledge with the help of the context given.
+        If the context given does not help, don't use the context.
+        Only output the questions.
+        
+        Output format= {output_format}
+        Context = {context}"""),
+        ("user","Create questions about {subject} with the topic {instruction}"),
+        ("ai", "Generated Questions=")
+]
 
 class LLMHandler:
     def __init__(self):
         self.is_initialized = False
+        self.model = None
         self.gpu_llm = None
-        self.initialize()
+        self.initialize_mistral()
 
-    def initialize(self):
+    def initialize_mistral(self):
         try:
+            self.gpu_llm = None
             self.gpu_llm = HuggingFacePipeline.from_model_id(
-                model_id="./Mistral-7B-Instruct-v0.2",
+                model_id="./data/Mistral-7B-Instruct-v0.2",
                 task="text-generation",  
                 device=0,  # replace with device_map="auto" to use the accelerate library.
                 pipeline_kwargs={"max_new_tokens": 3000} 
             )
-            self.is_initialized = True  
+            self.is_initialized = True
+            self.model = "Mistral"
+        except Exception as e:
+            print(f"An error occurred during initialization: {e}")
+            
+    def initialize_llama(self):
+        try:
+            self.gpu_llm = None
+            self.gpu_llm = HuggingFacePipeline.from_model_id(
+                model_id="./data/Meta-Llama-3-8B",
+                task="text-generation",  
+                device=0,  # replace with device_map="auto" to use the accelerate library.
+                pipeline_kwargs={"max_new_tokens": 3000} 
+            )
+            self.is_initialized = True 
+            self.model = "Llama"
         except Exception as e:
             print(f"An error occurred during initialization: {e}")
     
-    def chat_rag(self, extra, q_type, subject, lang, vector_db): 
-        retrieval = vector_db.similarity_search_with_score(subject, k=3)
-        context = "\n\n---\n\n".join([doc.page_content for doc, _score in retrieval])
-
-        if lang == "ENG":
-            template = templates[0][1]
-        elif lang == "INA":
-            template = templates[1][1]
-
-        if q_type == "single":
-            parser = JsonOutputParser(pydantic_object=MultipleChoice)
-        elif q_type == "text":
-            parser = JsonOutputParser(pydantic_object=Essay)
-        elif q_type == "multiple-answer":
-            parser = JsonOutputParser(pydantic_object=MultipleAnswer)
-
-
-        chat_template=ChatPromptTemplate.from_messages(template)
-        prompt = chat_template.format_messages(
-            amount = "5",
-            q_type=q_type, 
-            subject=subject, 
-            language=lang, 
-            extra=extra, 
-            context=context, 
-            format_instructions=parser.get_format_instructions()
-        )
+    def chat_rag(self, q_type, instruction, subject, lang, vector_db):   
+        retrieval = vector_db.similarity_search_with_score(instruction, k=3)
+        chunks = "\n---\n".join([doc.page_content for doc, _score in retrieval])
         
-        model = self.gpu_llm
-        response_text = model.invoke(prompt)
-
-        formatted_response = f"{response_text}"
-        return(formatted_response)
+        if q_type == "single":
+            output_parser = JsonOutputParser(pydantic_object=SingleAnswer)
+        elif q_type == "multiple":
+            output_parser = JsonOutputParser(pydantic_object=MultipleAnswer)
+        else:
+            output_parser = JsonOutputParser(pydantic_object=Essay)
+        
+        chat_template = ChatPromptTemplate.from_messages(template)
+        chat_template = chat_template.partial(amount="5", context=chunks, 
+                                              output_format=output_parser.get_format_instructions())
+        
+        chain = chat_template | self.gpu_llm
+        
+        return(chain.invoke({
+            "subject": subject,
+            "instruction": instruction,
+            "language": lang
+        }))
     
     def chat(self, query):
         model = self.gpu_llm
         response_text = model.invoke(query)
+        return (response_text)
+    
+    def joke(self, query) -> dict:
+        class Joke(BaseModel):
+            setup: str = Field(description="question to set up a joke")
+            punchline: str = Field(description="answer to resolve the joke")
+
+        model = self.gpu_llm
+
+        # Set up a parser + inject instructions into the prompt template.
+        parser = JsonOutputParser(pydantic_object=Joke)
+
+        prompt = PromptTemplate(
+            template="Answer the user query.\n{format_instructions}\n{query}\n",
+            input_variables=["query"],
+            partial_variables={"format_instructions": parser.get_format_instructions()},
+        )
+
+        chain = prompt | model | parser
+        response_text = chain.invoke({"query": query})
         return (response_text)
